8d5bfb4378e6d7f8e0e2b38e47bfeab5b9b9f2c1
Dragonchu <43578886+Dragonchu@users.noreply.github.com>
2023-07-14 02:35:08 +0000

    修改communication_element元素

--- src/aiharmonize/harmonizeai/communication_element.py
+++ src/aiharmonize/harmonizeai/communication_element.py
@@ -7,9 +7,6 @@ class FunctionPoint(BaseModel):
     param: list[str] = Field(description="Describes the definition of each parameter.")
     return_value: str = Field(description="Describes the return value of the function.")
     function_name: str = Field(description="Displays the name of the function.")
-    function_type: str = Field(description="Displays the type of the function.")
-    function_file: str = Field(description="Displays the file of the function.")
-    function_module: str = Field(description="Displays the module of the function.")
     function_class: str = Field(description="Displays the class of the function.")
 
 
